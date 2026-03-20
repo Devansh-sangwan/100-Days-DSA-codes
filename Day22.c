90336f00ca8bf1c0237c836b6cf285619b776708
Devansh-sangwan <sangwandevansh68@gmail.com>
2026-03-20 22:06:49 +0530

    code

--- Day22.c
+++ Day22.c
@@ -1,77 +1,40 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 struct Node {
     int data;
     struct Node* next;
 };
 
-struct Node* createNode(int data) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->data = data;
-    newNode->next = NULL;
-    return newNode;
-}
-
-struct Node* mergeSortedLists(struct Node* l1, struct Node* l2) {
-    struct Node dummy;
-    struct Node* tail = &dummy;
-    dummy.next = NULL;
-    
-    while (l1 != NULL && l2 != NULL) {
-        if (l1->data <= l2->data) {
-            tail->next = l1;
-            l1 = l1->next;
-        } else {
-            tail->next = l2;
-            l2 = l2->next;
-        }
-        tail = tail->next;
-    }
-    
-    tail->next = (l1 != NULL) ? l1 : l2;
-    return dummy.next;
-}
-
-void printList(struct Node* head) {
+int countNodes(struct Node* head) {
+    int count = 0;
     while (head != NULL) {
-        printf("%d", head->data);
-        if (head->next != NULL) printf(" ");
+        count++;
         head = head->next;
     }
-    printf("\n");
+    return count;
 }
 
 int main() {
-    int n, m, value;
-    struct Node *list1 = NULL, *list2 = NULL;
-    struct Node *tail1 = NULL, *tail2 = NULL;
-    
+    int n, value;
     scanf("%d", &n);
+    
+    struct Node* head = NULL;
+    struct Node* tail = NULL;
+    
     for (int i = 0; i < n; i++) {
         scanf("%d", &value);
-        struct Node* newNode = createNode(value);
-        if (list1 == NULL) {
-            list1 = tail1 = newNode;
+        struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = value;
+        newNode->next = NULL;
+        
+        if (head == NULL) {
+            head = tail = newNode;
         } else {
-            tail1->next = newNode;
-            tail1 = newNode;
+            tail->next = newNode;
+            tail = newNode;
         }
     }
     
-    scanf("%d", &m);
-    for (int i = 0; i < m; i++) {
-        scanf("%d", &value);
-        struct Node* newNode = createNode(value);
-        if (list2 == NULL) {
-            list2 = tail2 = newNode;
-        } else {
-            tail2->next = newNode;
-            tail2 = newNode;
-        }
-    }
-    
-    struct Node* merged = mergeSortedLists(list1, list2);
-    printList(merged);
+    printf("%d\n", countNodes(head));
     return 0;
 }
