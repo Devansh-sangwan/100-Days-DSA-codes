0cb7d0ad3f67fc0b4001b081857868094d9213e9
Devansh-sangwan <sangwandevansh68@gmail.com>
2026-04-01 21:24:50 +0530

    code

---
+++ Day48.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Definition of tree node
+struct TreeNode {
+    int val;
+    struct TreeNode* left;
+    struct TreeNode* right;
+};
+
+// Function to create a new node
+struct TreeNode* createNode(int value) {
+    struct TreeNode* newNode = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    newNode->val = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+// Function to count leaf nodes
+int countLeafNodes(struct TreeNode* root) {
+    if (root == NULL)
+        return 0;
+
+    // Check if leaf node
+    if (root->left == NULL && root->right == NULL)
+        return 1;
+
+    return countLeafNodes(root->left) + countLeafNodes(root->right);
+}
+
+int main() {
+    
+    struct TreeNode* root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+    root->right->right = createNode(7);
+
+    int result = countLeafNodes(root);
+
+    printf("Number of leaf nodes = %d\n", result);
+
+    return 0;
+}
